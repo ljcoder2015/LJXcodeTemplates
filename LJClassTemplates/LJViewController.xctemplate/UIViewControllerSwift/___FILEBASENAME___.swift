2cbeaa216e09fce934a1cfeefa78980c9d69136c
ljcoder <ljcoder@163.com>
2017-06-21 17:55:52 +0800

    LJViewController,LJViewModel

---
+++ LJClassTemplates/LJViewController.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,51 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//  GitHub: https://github.com/ljcoder2015
+//
+
+import UIKit
+#if !RX_NO_MODULE
+    import RxSwift
+    import RxCocoa
+#endif
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+    // MARK: UI
+    
+    // MARK: Property
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = <#title#>
+        
+        setupUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: bindViewModel
+    override func bindViewModel() {
+        
+    }
+}
+
+// MARK: SetupUI
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    fileprivate func setupUI() {
+        // addSubView
+        
+        // layout
+        
+    }
+}
---
+++ LJClassTemplates/LJViewModel.xctemplate/NSObjectSwift/___FILEBASENAME___.swift
@@ -0,0 +1,42 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//  GitHub: https://github.com/ljcoder2015
+//
+
+import UIKit
+#if !RX_NO_MODULE
+    import RxSwift
+    import RxCocoa
+#endif
+import Alamofire
+import HandyJSON
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+    // MARK: output
+    
+    // MAKR: API
+    
+    // MARK: input
+    
+    // MARK: property
+    
+    // MAKR: init
+    init() { }
+    
+}
+
+// MAKR: Data Request
+extension ___FILEBASENAMEASIDENTIFIER___: LJRequest, LJRequestCallBack {
+    
+    func route(_ api: LJBaseAPI) -> String {
+        return ""
+    }
+
+    func callSuccess(_ api: LJBaseAPI, result: LJResponse) {
+
+    }
+}
