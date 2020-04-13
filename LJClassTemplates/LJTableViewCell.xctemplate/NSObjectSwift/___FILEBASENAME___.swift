ff0444988ae3290bd21a6b87ece679bbe02499f2
ljcoder <ljcoder@163.com>
2020-04-13 15:47:17 +0800

    add tableView Cell

---
+++ LJClassTemplates/LJTableViewCell.xctemplate/NSObjectSwift/___FILEBASENAME___.swift
@@ -0,0 +1,33 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//  GitHub: https://github.com/ljcoder2015
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UITableViewCell {
+
+    // UI
+
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    fileprivate func setupUI() {
+        
+    }
+}
