4fe1cf789ad63b883e6bbbb517be18ab357d06f4
leijun <ljcoder@163.com>
2022-12-07 10:09:54 +0800

    feat: 拆分VC ViewModel Model

---
+++ LJViewController.xctemplate/___FILEBASENAME___VC.swift
@@ -0,0 +1,57 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//  GitHub: https://github.com/ljcoder2015
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ___FILEBASENAMEASIDENTIFIER___: LJBaseViewController {
+
+    // MARK: UI
+    
+    // MARK: Input
+    
+    // MARK: Output
+    
+    // MARK: Properties
+    
+    // MARK: Init
+}
+
+// MARK: Life Cycle
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = <#title#>
+        
+        setupUI()
+        
+        bindViewModel()
+    }
+}
+
+// MARK: bindViewModel
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    private func bindViewModel() {
+        
+    }
+}
+
+// MARK: SetupUI
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    private func setupUI() {
+        // addSubView
+        
+        // layout
+        
+    }
+}
---
+++ LJViewModel.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//  GitHub: https://github.com/ljcoder2015
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Moya
+
+class ___FILEBASENAMEASIDENTIFIER___: LJBaseViewModel {
+    // MARK: Output
+    private(set) var list: [<#Model#>] = []
+    private var page: Int = 1
+    // MARK: Driver
+    lazy var listDriver: Driver<[<#Model#>]> = self.listPublishRelay.asDriver(onErrorJustReturn:[])
+    lazy var moreDriver: Driver<Bool> = self.morePublishRelay.asDriver(onErrorJustReturn: false)
+
+    // MARK: PublishRelay
+    private var listPublishRelay = PublishRelay<[<#Model#>]>()
+    private var morePublishRelay = PublishRelay<Bool>()
+    
+}
+
+// MARK: Action
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    func fetchList() {
+        Api.request(<#API#>)
+            .asObservable()
+            .mapObject(LJListModel<<#Model#>>.self)
+            .filterHUDError()
+            .subscribe(onNext: { (result) in
+                self.listPublishRelay.accept(result)
+                // 分页是否到最后一页判断
+                if result.data?.currentPage ?? 0 < result.data?.lastPage ?? 0 {
+                    self.morePublishRelay.accept(true)
+                }
+                else {
+                    self.morePublishRelay.accept(false)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
